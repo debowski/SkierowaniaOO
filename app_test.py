a7da5feeb2fad595b711fc12430df32fdccb6f47
debowski <piotr.debowski@zseiulaziska.pl>
2023-04-07 13:41:37 +0200

    Update app_test.py
    3

--- app_test.py
+++ app_test.py
@@ -1,10 +1,15 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import ttkbootstrap as ttkb
 
 class App:
     def __init__(self):
+
+        self.napis1 = "to jest test"
+
+
         self.root = tk.Tk()
-        self.root.title("Widzety")
+        self.root.title("Skierowania 0.22")
         self.root.geometry("300x300")
         self.root.grid()
         self.root.columnconfigure(0, weight=1)
@@ -16,16 +21,16 @@ class App:
 
 
     def dodaj_widzety(self):
-        self.frame = tk.Frame(self.root, background="red")
+        self.frame = ttkb.Frame(self.root, bootstyle="info")
         self.frame.grid(row=0, column=0, sticky="nsew")
 
         self.frame.columnconfigure(0, weight=1)
 
-        self.button = ttk.Button(self.frame, text="Dodaj widzety", command=self.ble)
+        self.button = ttkb.Button(self.frame, text="Wybierz plik", command=self.ble)
         self.button.grid(row=0, column=0, sticky="nsew")
     
     def ble(self):
-        print("test")
+        print(self.napis1)
 
 
 if __name__ == "__main__":
