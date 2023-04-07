04c31930e8a68260cd5a4717b739081b0f2c2c41
debowski <piotr.debowski@zseiulaziska.pl>
2023-04-07 13:27:04 +0200

    2
    2

---
+++ app_test.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+class App:
+    def __init__(self):
+        self.root = tk.Tk()
+
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.root.mainloop()
+
+
