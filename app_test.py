2038811189586cb65bcb2c22e056f6d8e9bba34b
debowski <piotr.debowski@zseiulaziska.pl>
2023-04-07 13:35:17 +0200

    3
    3

--- app_test.py
+++ app_test.py
@@ -4,8 +4,28 @@ import tkinter.ttk as ttk
 class App:
     def __init__(self):
         self.root = tk.Tk()
+        self.root.title("Widzety")
+        self.root.geometry("300x300")
+        self.root.grid()
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
+        self.dodaj_widzety()
 
 
+
+
+    def dodaj_widzety(self):
+        self.frame = tk.Frame(self.root, background="red")
+        self.frame.grid(row=0, column=0, sticky="nsew")
+
+        self.frame.columnconfigure(0, weight=1)
+
+        self.button = ttk.Button(self.frame, text="Dodaj widzety", command=self.ble)
+        self.button.grid(row=0, column=0, sticky="nsew")
+    
+    def ble(self):
+        print("test")
 
 
 if __name__ == "__main__":
